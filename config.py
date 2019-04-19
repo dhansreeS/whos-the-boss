2034db22f11b2a900815d2b99ca07264400514c9
Dhansree Suraj <dhansreesuraj2019@u.northwestern.edu>
2019-04-19 09:50:40 -0500

    Updated port number

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DEBUG = True
 LOGGING_CONFIG = "config/logging/local.conf"
-PORT = 3000
-APP_NAME = "penny-lane"
+PORT = 9033
+APP_NAME = "whos-the-boss"
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/tracks.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 HOST = "127.0.0.1"
