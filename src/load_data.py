84d968f4aa93b40de96ab73e8c952f92ef7f5762
Dhansree Suraj <dhansreesuraj2019@u.northwestern.edu>
2019-05-14 18:35:32 -0500

    Updated bucket name in load_data

--- src/load_data.py
+++ src/load_data.py
@@ -3,5 +3,5 @@ import boto3
 s3 = boto3.resource('s3')
 
 copy_source = {'Bucket': 'nw-dhansreesuraj-s3', 'Key': 'the_office_lines.csv'}
-bucket = s3.Bucket('bucket_boss')
+bucket = s3.Bucket('bucket-boss')
 bucket.copy(copy_source, 'the_office_lines.csv')
