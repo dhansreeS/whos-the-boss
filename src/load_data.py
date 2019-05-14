2a656285d0cc4ae8b50d0fa70fb30708f8cba7f8
Dhansree Suraj <dhansreesuraj2019@u.northwestern.edu>
2019-05-14 18:27:12 -0500

    updated load_data

--- src/load_data.py
+++ src/load_data.py
@@ -4,4 +4,4 @@ s3 = boto3.resource('s3')
 
 copy_source = {'Bucket': 'nw-dhansreesuraj-s3'}
 bucket = s3.Bucket('bucket_boss')
-bucket.copy(copy_source)
+bucket.copy(copy_source, '/raw/the-office-lines.csv')
