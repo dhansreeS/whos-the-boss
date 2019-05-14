40d571f878c5a11a86349bd27e7f4e6915cc18c5
Dhansree Suraj <dhansreesuraj2019@u.northwestern.edu>
2019-05-14 18:31:15 -0500

    Added load_data key

--- src/load_data.py
+++ src/load_data.py
@@ -2,6 +2,6 @@ import boto3
 
 s3 = boto3.resource('s3')
 
-copy_source = {'Bucket': 'nw-dhansreesuraj-s3', 'Key': '/raw/the-office-lines.csv'}
+copy_source = {'Bucket': 'nw-dhansreesuraj-s3', 'Key': 'the-office-lines.csv'}
 bucket = s3.Bucket('bucket_boss')
-bucket.copy(copy_source, '/raw/the-office-lines.csv')
+bucket.copy(copy_source, 'the-office-lines.csv')
