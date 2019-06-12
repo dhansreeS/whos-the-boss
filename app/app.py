6a7352cf2d56ff6dadd692a47070c8e6d0c3a259
Dhansree Suraj <dhansreesuraj2019@u.northwestern.edu>
2019-06-12 15:08:40 -0500

    Update app.py vectorizer and model

--- app/app.py
+++ app/app.py
@@ -34,22 +34,6 @@ except FileNotFoundError:
     logger.error('config YAML File not Found')
     sys.exit(1)
 
-# load models - tfidf
-
-if app.config['USE_S3']:
-    s3 = boto3.resource('s3')
-    with io.BytesIO() as data:
-        s3.Bucket(app.config['DEST_S3_BUCKET']).download_fileobj(app.config['S3_TFIDF'], data)
-        data.seek(0)  # move back to the beginning after writing
-        vectorizer = pickle.load(data)
-    with io.BytesIO() as data2:
-        s3.Bucket(app.config['DEST_S3_BUCKET']).download_fileobj(app.config['S3_MODEL'], data2)
-        data2.seek(0)  # move back to the beginning after writing
-        model = pickle.load(data2)
-else:
-    vectorizer = pickle.load(open(app.config['TFIDF_PATH'], 'rb'))
-    model = pickle.load(open(app.config['MODEL_PATH'], 'rb'))
-
 if app.config['USE_RDS']:
     aws_config = config['rds']
 
@@ -62,7 +46,6 @@ if app.config['USE_RDS']:
 
     app.config['SQLALCHEMY_DATABASE_URI'] = '{}://{}:{}@{}:{}/{}'. \
         format(conn_type, user, password, host, port, database)
-
 
 db = SQLAlchemy(app)
 
@@ -98,6 +81,22 @@ def main():
             statement = str(request.form['statement'])
 
             processed = process_data(statement)
+            # load models - tfidf
+
+            if app.config['USE_S3']:
+                s3 = boto3.resource('s3')
+                with io.BytesIO() as data:
+                    s3.Bucket(app.config['DEST_S3_BUCKET']).download_fileobj(app.config['S3_TFIDF'], data)
+                    data.seek(0)  # move back to the beginning after writing
+                    vectorizer = pickle.load(data)
+                with io.BytesIO() as data2:
+                    s3.Bucket(app.config['DEST_S3_BUCKET']).download_fileobj(app.config['S3_MODEL'], data2)
+                    data2.seek(0)  # move back to the beginning after writing
+                    model = pickle.load(data2)
+            else:
+                vectorizer = pickle.load(open(app.config['TFIDF_PATH'], 'rb'))
+                model = pickle.load(open(app.config['MODEL_PATH'], 'rb'))
+
             processed = vectorizer.transform(processed)
 
             prediction = model.predict(processed)
